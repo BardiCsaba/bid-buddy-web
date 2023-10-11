28f291a790ebe9caa0d069415474d6e467386c2d
BardiCsaba <68243976+BardiCsaba@users.noreply.github.com>
2023-10-11 17:09:09 +0200

    added mobile responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'bid-buddy-web';
+    public menuOpen = false;
+
+    toggleMenu() {
+        this.menuOpen = !this.menuOpen;
+    }
+
 }
