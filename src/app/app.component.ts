8ff0a9784198d511db6e77e9ca262672bc3e33f5
BardiCsaba <68243976+BardiCsaba@users.noreply.github.com>
2023-10-12 13:02:53 +0200

    Added Profile to sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -8,6 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'bid-buddy-web';
     public menuOpen = false;
+    profilePicUrl = '/assets/images/test.jpg';
+    username = 'User Name';
+    
+    constructor(private router: Router) {}
+
+    goToProfile(): void {
+        this.router.navigate(['/profile']);
+    }
 
     toggleMenu() {
         this.menuOpen = !this.menuOpen;
